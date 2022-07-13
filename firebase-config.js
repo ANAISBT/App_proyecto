72c2f9e4f0907c5a507a30f2b1f26a0cf0779966
ANAIS <anais.bustamante@unmsm.edu.pe>
2022-07-13 10:25:06 -0500

    add cod

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+//  TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyAQEMq1UY6AJAgIz0y5PaB7MG4rylHV6AQ",
+    authDomain: "proyectfinal-c15d8.firebaseapp.com",
+    projectId: "proyectfinal-c15d8",
+    storageBucket: "proyectfinal-c15d8.appspot.com",
+    messagingSenderId: "54309128226",
+    appId: "1:54309128226:web:3868fdb236f7f8b8b8e27b"
+  };
+  
